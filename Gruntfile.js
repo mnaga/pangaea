98591f4a81f140b6c7e925acfb7d0d54de811f70
Ryan Eastridge <ryan@eastridge.me>
2013-03-20 15:35:55 -0700

    Add thorax inspector

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,34 @@
 module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-bg-shell');
+  grunt.loadNpmTasks('thorax-inspector');
 
   grunt.initConfig({
+    // allows files to be opened when the
+    // Thorax Inspector Chrome extension
+    // is installed
+    thorax: {
+      inspector: {
+        background: true,
+        editor: "subl",
+        livereload: {
+          base: './build/dev',
+          exts: [
+            'html',
+            'css',
+            'jpg',
+            'png',
+            'gif',
+            'js'
+          ]
+        },
+        paths: {
+          views: "./js/views",
+          models: "./js/models",
+          collections: "./js/collections",
+          templates: "./templates"
+        }
+      }
+    },
     bgShell: {
       _defaults: {
         bg: true
@@ -21,12 +48,13 @@ module.exports = function(grunt) {
   });
 
   grunt.registerTask('default', [
+    'thorax:inspector',
     'bgShell:mockData',
     'bgShell:phoenixWatch'
   ]);
 
   grunt.registerTask('production', [
-    'mockData',
+    'bgShell:mockData',
     'phoenixStart'
   ]);
 };
